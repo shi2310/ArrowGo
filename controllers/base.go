7fd4d0fc7bcda1f41cc256531452dd7750064843
wan2310 <shimon.shi@bst.ai>
2020-06-04 15:25:06 +0800

    文件上传

--- controllers/base.go
+++ controllers/base.go
@@ -4,9 +4,10 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"errors"
-	"io"
+	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 
 	"ArrowGo/models"
 
@@ -14,16 +15,52 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-// Upload 公用上传文件方法
-func Upload(context *gin.Context, field string) (*models.File, error) {
+// FormUpload Form上传文件方法
+func FormUpload(context *gin.Context, field string) (*models.File, error) {
 	// 获取FormData中文件
-	file, header, err := context.Request.FormFile(field)
+	header, err := context.FormFile(field)
 	if err != nil {
 		return nil, err
 	}
-	// 获取文件的MD5
+	oldFileName := header.Filename
+	ext := path.Ext(oldFileName)
+
+	file, err := header.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	// if err := context.SaveUploadedFile(header, filepath); err != nil {
+	// 	return nil, errors.New("文件保存出错")
+	// }
+	return saveFile(ext, bytes)
+}
+
+// Base64Upload Base64上传文件方法
+func Base64Upload(context *gin.Context) (*models.File, error) {
+	bytes, err := ioutil.ReadAll(context.Request.Body)
+	if err != nil {
+		return nil, err
+	}
+	strs := strings.Split(string(bytes), ",")
+	head := strs[0]
+	body := strs[1]
+	start := strings.LastIndex(head, "/")
+	end := strings.LastIndex(head, ";")
+	ext := head[start+1 : end]
+
+	return saveFile(ext, []byte(body))
+}
+
+// saveFile 私有方法
+func saveFile(ext string, bytes []byte) (*models.File, error) {
+	// 获取文件MD5
 	md5 := md5.New()
-	io.Copy(md5, file)
+	md5.Write(bytes)
 	fileMd5 := hex.EncodeToString(md5.Sum(nil))
 	v, err := models.GetFileByMD5(fileMd5)
 	if err != nil {
@@ -35,21 +72,20 @@ func Upload(context *gin.Context, field string) (*models.File, error) {
 				os.Mkdir(folder, os.ModePerm)
 			}
 		}
-		oldFileName := header.Filename
-		ext := path.Ext(oldFileName)
+
+		// 重设文件名
 		_uuid := uuid.NewV4()
 		filename := _uuid.String() + ext
 		filepath := path.Join(folder, filename)
 
-		if err := context.SaveUploadedFile(header, filepath); err != nil {
+		if err = ioutil.WriteFile(filepath, bytes, 0666); err != nil {
 			return nil, errors.New("文件保存出错")
 		}
 
 		v = &models.File{Name: filename, URL: filepath, FileMd5: fileMd5}
 		if err := models.AddFile(v); err != nil {
-			return nil, errors.New("submit file " + oldFileName + " record failed")
+			return nil, errors.New("submit file record failed")
 		}
-
 	}
 	return v, nil
 }
